a25a8ca5f167321ebbd1e435115142fea283e35d
LangYa1945 <1347150711@qq.com>
2018-08-10 09:17:30 +0800

    第二次提交

--- hutubill/src/service/RecordService.java
+++ hutubill/src/service/RecordService.java
@@ -8,7 +8,7 @@ import entity.Record;
  
 public class RecordService {
     RecordDAO recordDao = new RecordDAO();
-    //根据消费金额，消费分类，备注和日期添加一条消费记录
+    //根据消费金额1，消费分类，备注和日期添加一条消费记录
     public void add(int spend, Category c, String comment,Date date){
         Record r = new Record();
         r.spend = spend;
